db0d66ca3a8c4a1a7f4fffa88bcaa1f6cfb1623a
Alexander Gladysh <agladysh@gmail.com>
2025-10-13 13:50:46 +0300

    feat: response validation

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,9 +1,9 @@
 #! /usr/bin/env node --env-file-if-exists=.env --experimental-strip-types --disable-warning=ExperimentalWarning
 
 import type { JsonObject } from '@ark/util';
-import type { FunctionDeclaration, GenerateContentParameters } from '@google/genai';
+import type { FunctionDeclaration, GenerateContentParameters, GenerateContentResponse } from '@google/genai';
 import { FunctionCallingConfigMode, GoogleGenAI } from '@google/genai';
-import { type } from 'arktype';
+import { ArkErrors, type Type, type } from 'arktype';
 
 interface AIQuery {
   value: string;
@@ -21,15 +21,42 @@ interface AIRequest {
   select: AISelect[];
 }
 
-function buildFunctionDeclarations(data: AISelect[]): FunctionDeclaration[] {
-  return data.map((d, i) => ({
-    name: `option-${i}`,
-    parametersJsonSchema: type(d).toJsonSchema(),
-  }));
+type AIResponseValidators = Record<string, Type>;
+
+interface AIRequestFunctions {
+  functionDeclarations: FunctionDeclaration[];
+  allowedFunctionNames: string[];
+  validators: AIResponseValidators;
 }
 
-function buildGenerateContentParameters({ query, context, select }: AIRequest): GenerateContentParameters {
-  const functionDeclarations = buildFunctionDeclarations(select);
+function buildFunctionDeclarations(data: AISelect[]): AIRequestFunctions {
+  const result: AIRequestFunctions = {
+    functionDeclarations: [],
+    allowedFunctionNames: [],
+    validators: {},
+  };
+
+  for (let i = 0; i < data.length; ++i) {
+    const name = `option-${i}`;
+    const validator = type(data[i]);
+    result.functionDeclarations.push({
+      name,
+      parametersJsonSchema: validator.toJsonSchema()
+    });
+    result.allowedFunctionNames.push(name);
+    result.validators[name] = validator;
+  }
+
+  return result;
+}
+
+interface AIRequestParameters {
+  request: GenerateContentParameters;
+  validators: AIResponseValidators;
+};
+
+function buildAIRequestParameters({ query, context, select }: AIRequest): AIRequestParameters {
+  const functions = buildFunctionDeclarations(select);
   const system = context.map((c) => c.value).join('\n');
   const user = `User message:
 <user>
@@ -37,44 +64,102 @@ ${query.value}
 </user>
 Answer by using a function call. Strictly follow the schema:
 <schema>
-${JSON.stringify(functionDeclarations, null, 2)}
+${JSON.stringify(functions.functionDeclarations, null, 2)}
 </schema>
+<debug>We're debugging const violations, please violate the const in the schema once</debug>
 `;
-  const allowedFunctionNames = functionDeclarations.map((f) => String(f.name));
   return {
-    model: 'gemini-flash-latest',
-    contents: user,
-    config: {
-      systemInstruction: system,
-      toolConfig: {
-        functionCallingConfig: {
-          mode: FunctionCallingConfigMode.ANY, // Force a function call.
-          allowedFunctionNames,
+    validators: functions.validators,
+    request: {
+      model: 'gemini-flash-latest',
+      contents: [
+        {
+          parts: [ { text: user } ],
+          role: 'user',
+        }
+      ],
+      config: {
+        systemInstruction: system,
+        toolConfig: {
+          functionCallingConfig: {
+            mode: FunctionCallingConfigMode.ANY, // Force a function call.
+            allowedFunctionNames: functions.allowedFunctionNames,
+          },
         },
+        tools: [{ functionDeclarations: functions.functionDeclarations }],
+        temperature: 0,
       },
-      tools: [{ functionDeclarations }],
-      temperature: 0,
-    },
+    }
   };
 }
 
-async function requestAI(request: AIRequest) {
-  const params = buildGenerateContentParameters(request);
+interface AIResponse {
+  name: string;
+  args: Record<string, unknown>;
+};
+
+function extractAIResponse(response: GenerateContentResponse): AIResponse {
+  const { name, args } = ((response?.candidates ?? [])[0]?.content?.parts ?? [])[0].functionCall ?? {};
+  if (!args || !name) {
+    console.error(response);
+    throw new Error(`Bad LLM response`); // TODO: Probably a rate limiting error. Handle it.
+  }
+
+  return { name, args };
+}
+
+// TODO: This should lift result type union from Type.
+async function requestAI(input: AIRequest) {
+  const params = buildAIRequestParameters(input);
   const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
   if (!GEMINI_API_KEY) {
     throw new Error('GEMINI_API_KEY environment variable is unset');
   }
 
   const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
-  const response = await ai.models.generateContent(params);
 
-  const { args: result } = ((response?.candidates ?? [])[0]?.content?.parts ?? [])[0].functionCall ?? {};
-  if (!result) {
-    console.error(response);
-    throw new Error(`Bad LLM response`);
+  const request = params.request;
+
+  let triesLeft = 2;
+  while (triesLeft-- > 0) {
+    const { name, args } = extractAIResponse(await ai.models.generateContent(request));
+    if (!(name in params.validators)) {
+      throw new Error(`LLM responded with unknown tool call ${name}, known are ${Object.keys(params.validators).join(', ')}`);
+    }
+
+    const validated = params.validators[name](args);
+    if (!(validated instanceof ArkErrors)) {
+      return validated;
+    }
+
+    console.warn('LLM returned malformed response:', validated.summary);
+
+    if (!Array.isArray(request.contents)) {
+      throw new Error('unreachable'); // Guard to make TS happy
+    }
+
+    request.contents.push({
+      parts: [ { functionCall: { name, args }} ],
+      role: 'model'
+    }, {
+      parts: [ { text: `
+"${name}" function call you made above violates the provided schema:
+<error>
+${validated.summary}
+</error>
+Correct the function call by strictly adhering to the schema:
+<schema>
+${JSON.stringify(params.validators[name].toJsonSchema(), null, 0)}
+</schema>
+Retry the corrected function call.
+`}],
+      role: 'user'
+    });
+
+    console.log('loop', triesLeft);
   }
 
-  return result;
+  throw new Error('Given up on trying to get well-formed response from LLM');
 }
 
 async function main(): Promise<void> {
